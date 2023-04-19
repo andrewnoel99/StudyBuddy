c1c62158ef0f36dc126e4017f0bd9c1a7aa970f1
Andrew Noel <noel010@csusm.edu>
2023-04-19 15:43:53 -0700

    4/19 3:43pm

--- app/src/main/java/com/example/studybuddy20/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy20/MainActivity.kt
@@ -1,33 +1,67 @@
 package com.example.studybuddy20
 
-import android.annotation.TargetApi
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mathButton: Button
+    private lateinit var csButton: Button
+    private lateinit var startButton: Button
+    private lateinit var countdownTV: TextView
+
+    private var countdownLength: Long = 0
+    private var countdown: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var counter = 0
 
+        mathButton = findViewById(R.id.Mathbtn)
+        csButton = findViewById(R.id.CSbtn)
+        startButton = findViewById(R.id.StartBtn)
+        countdownTV = findViewById(R.id.countdownTV)
 
-        //@TargetApi(Build.VERSION_CODES.O)
+        mathButton.setOnClickListener(this)
+        csButton.setOnClickListener(this)
+        startButton.setOnClickListener(this)
 
+        countdownTV.text = "0"
+    }
 
-
-            val counttime = findViewById<TextView>(R.id.textVeiw2)
-            object : CountDownTimer(50000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    counttime.text = counter.toString()
-                    counter++
-                }
-
-                override fun onFinish() {
-                    counttime.text = "Finished"
-                }
-            }.start()
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.Mathbtn -> {
+                countdownLength = 20 * 60 * 1000 // 20 minutes
+                countdownTV.text = "20:00"
+            }
+            R.id.CSbtn -> {
+                countdownLength = 15 * 60 * 1000 // 15 minutes
+                countdownTV.text = "15:00"
+            }
+            R.id.StartBtn -> startCountdown()
         }
     }
+
+    private fun startCountdown() {
+        countdown?.cancel()
+
+        countdown = object : CountDownTimer(countdownLength, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val minutes = millisUntilFinished / 1000 / 60
+                val seconds = (millisUntilFinished / 1000) % 60
+
+                val formattedTime = String.format("%02d:%02d", minutes, seconds)
+                countdownTV.text = formattedTime
+            }
+
+            override fun onFinish() {
+                countdownTV.text = "00:00"
+            }
+        }.start()
+    }
+}
