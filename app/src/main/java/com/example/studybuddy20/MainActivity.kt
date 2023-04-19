f9dabdf451bd53da8e00bb7008a6bee032f81510
Andrew Noel <noel010@csusm.edu>
2023-04-19 12:33:54 -0700

    12:33pm 4/19

--- app/src/main/java/com/example/studybuddy20/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy20/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.studybuddy20
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var counter = 0
+
+        @SuppressLint("MissingInflatedId")
+        @TargetApi(Build.VERSION_CODES.O)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val counttime = findViewById<TextView>(R.id.counttime)
+            object : CountDownTimer(50000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    counttime.text = counter.toString()
+                    counter++
+                }
+
+                override fun onFinish() {
+                    counttime.text = "Finished"
+                }
+            }.start()
+        }
     }
-}
+}}
